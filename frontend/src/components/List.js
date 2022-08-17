7e94f76a0763a38c07868591be192092b991a6f4
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-08-17 21:00:13 +0530

    till list item

---
+++ frontend/src/components/List.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul className="doc__lists">
+      <li className="doc__list__item">List</li>
+    </ul>
+  );
+}
+
+export default List;
---
+++ frontend/src/pages/ViewPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import List from "../components/List";
+
+function ViewPage() {
+  return (
+    <div>
+      <div>ViewPage</div> <List />
+    </div>
+  );
+}
+
+export default ViewPage;
