eb305807e0098d119e8c84742a48de4600769f35
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-08-18 20:10:05 +0530

    upload system

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Form } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 function Doc({ name }) {
   return (
--- frontend/src/pages/ViewPage.js
+++ frontend/src/pages/ViewPage.js
@@ -1,14 +1,40 @@
 import React from "react";
 import List from "../components/List";
+import { Button, Form } from "react-bootstrap";
+
+function UploadDoc() {
+  return (
+    <Form onSubmit={() => {}}>
+      <Form.Group style={{ padding: "30px" }}>
+        <Form.Label>
+          <b>Upload Document</b>
+        </Form.Label>
+        <Form.Control
+          style={{ padding: "25px" }}
+          type="text"
+          className="input"
+          // value={value}
+          onChange={() => {}}
+          placeholder="Upload Document"
+        />
+      </Form.Group>
+
+      <Button variant="primary mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
 
 function ViewPage() {
   return (
     <div className="my__view">
       <div className="container">
         <h1 className="text-center mb-4">View Page</h1>
-      </div>
-      <div>
-        <List />
+        <UploadDoc />
+        <div>
+          <List />
+        </div>
       </div>
     </div>
   );
